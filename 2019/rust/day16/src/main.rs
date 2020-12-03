46f99f97c045e253f601d6c91864d158f235b73a
Tubbles <jae91m@gmail.com>
2020-12-03 18:53:23 +0100

    Day16 part1/2 WIP

--- 2019/rust/day16/src/main.rs
+++ 2019/rust/day16/src/main.rs
@@ -7,8 +7,8 @@ fn str_to_intvec(s: &str) -> Vec<isize> {
         .collect()
 }
 
-fn get_first_8(v: &Vec<isize>) -> Vec<isize> {
-    v.into_iter().take(8).map(|&a| a).collect()
+fn get_first_n_w_offset(v: &Vec<isize>, n: usize, offset: usize) -> Vec<isize> {
+    v.into_iter().skip(offset).take(n).map(|&a| a).collect()
 }
 
 fn phase(v: &Vec<isize>, iters: usize) -> Vec<isize> {
@@ -17,7 +17,8 @@ fn phase(v: &Vec<isize>, iters: usize) -> Vec<isize> {
     let mut outvec = Vec::new();
     let mut invec = v.clone();
 
-    for _ in 0..iters {
+    for iter in 0..iters {
+        println!("iter: {}", iter);
         outvec = invec.clone();
         for out_i in 0..invec.len() {
             for in_i in 0..invec.len() {
@@ -48,7 +49,7 @@ fn main() {
     for i in 0..small_examples_in.len() {
         println!();
         let res = phase(&str_to_intvec(small_examples_in[i]), 1);
-        let res = get_first_8(&res);
+        let res = get_first_n_w_offset(&res, 8, 0);
         let assert = str_to_intvec(small_examples_out[i]);
         println!("small example num {}: {:?} == {:?}", i + 1, res, assert);
         assert!(res == assert);
@@ -57,7 +58,7 @@ fn main() {
     for i in 0..long_examples_in.len() {
         println!();
         let res = phase(&str_to_intvec(long_examples_in[i]), 100);
-        let res = get_first_8(&res);
+        let res = get_first_n_w_offset(&res, 8, 0);
         let assert = str_to_intvec(long_examples_out[i]);
         println!("long example num {}: {:?} == {:?}", i + 1, res, assert);
         assert!(res == assert);
@@ -66,9 +67,32 @@ fn main() {
     let mut file = File::open("input.txt").expect("open file error");
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("read file error");
-    let res = phase(&str_to_intvec(&contents), 100);
-    let res = get_first_8(&res);
+    let vec = str_to_intvec(&contents);
+    let res = phase(&vec, 100);
+    let res = get_first_n_w_offset(&res, 8, 0);
 
     println!();
     println!("part1: {:?}", res);
+
+    println!("part2:");
+    let offset = get_first_n_w_offset(&vec, 7, 0)
+        .iter()
+        .fold(0, |acc, i| acc * 10 + *i as usize);
+    println!("offset: {}", offset);
+    let mut vec2 = vec.clone();
+    for _ in 0..10_000 - 1 {
+        vec2.extend(&vec);
+    }
+
+    println!("vec.len(): {}", vec.len());
+    println!("vec2.len(): {}", vec2.len());
+    println!(
+        "vec2 @ {}: {:?}",
+        offset,
+        get_first_n_w_offset(&vec2, 8, offset)
+    );
+
+    let res = phase(&vec2, 100);
+    let res = get_first_n_w_offset(&res, 8, offset);
+    println!("part2: {:?}", res);
 }
